633350f2c6ee5a96c247c76bd32775282c52b83c
Stefan <belciugstefan@gmail.com>
2026-01-25 05:59:08 +0000

    Initial commit

---
+++ src/main/java/com/company/promo_service/PromoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.company.promo_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromoServiceApplication.class, args);
+	}
+
+}
