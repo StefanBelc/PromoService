a4ba7da4989556997b2ef218c785e9f5c6fc47c4
Stefan <belciugstefan@gmail.com>
2026-01-25 12:50:36 +0000

    PA-28 - kafka configuration, game-events and tournament-events consumers

---
+++ src/main/java/com/company/promo_service/GameEventSubscriber.java
@@ -0,0 +1,17 @@
+package com.company.promo_service;
+
+import com.company.promobridge.GameEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+
+
+public class GameEventSubscriber {
+
+    private static final Logger logger = LoggerFactory.getLogger(GameEventSubscriber.class);
+
+    @KafkaListener(topics = "${game-events.topic}", groupId = "promo-service", concurrency = "${game-events.concurrency}")
+    public void onGameEvent(GameEvent event) {
+        logger.info("Received game event {}{}", event.gameId(), event);
+    }
+}
