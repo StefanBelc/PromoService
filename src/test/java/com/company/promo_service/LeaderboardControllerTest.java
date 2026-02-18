0ad7345baf45b6bb93a9ad538c5dd9ccbfb6ccfd
Stefan <belciugstefan@gmail.com>
2026-02-18 20:03:02 +0000

    PA-32 - add changes to leaderboard test class

--- src/test/java/com/company/promo_service/LeaderboardControllerTest.java
+++ src/test/java/com/company/promo_service/LeaderboardControllerTest.java
@@ -1,14 +1,17 @@
 package com.company.promo_service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -24,30 +27,50 @@ class LeaderboardControllerTest {
     @MockitoBean
     private LeaderboardService leaderboardService;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
 
     @Test
-    void getTop() throws Exception {
+    void should_returnLeaderboard_with_topNPlayers() throws Exception {
 
-        LeaderboardDto leaderboardDto = new LeaderboardDto(List.of(
+        LeaderboardDto expectedLeaderboard = new LeaderboardDto(List.of(
                 new LeaderboardEntryDto("Mihai", 10, 1),
                 new LeaderboardEntryDto("Alex", 8, 2),
                 new LeaderboardEntryDto("Florin", 7, 3)
         ));
 
-        when(leaderboardService.getTop("tournament1", 3)).thenReturn(leaderboardDto);
+        when(leaderboardService.getTop("tournament1", 3)).thenReturn(expectedLeaderboard);
 
-        mockMVc.perform(get("/tournament/tournament1/leaderboard/top/3"))
+        MvcResult result = mockMVc.perform(get("/tournament/tournament1/leaderboard/top/3"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.leaderboardEntryDtoList[0].name").value("Mihai"))
-                .andExpect(jsonPath("$.leaderboardEntryDtoList[1].score").value(8))
-                .andExpect(jsonPath("$.leaderboardEntryDtoList[2].rank").value(3));
+                .andReturn();
+
+        String response = result.getResponse().getContentAsString();
+        LeaderboardDto actualLeaderboard = objectMapper.readValue(response, LeaderboardDto.class);
+
+        assertThat(actualLeaderboard)
+                .usingRecursiveComparison()
+                .isEqualTo(expectedLeaderboard);
+
+
+    }
+
+    @Test
+    void should_return404_when_TournamentNotFound() throws Exception {
+        when(leaderboardService.getTop("invalid", 2)).thenThrow(new TournamentNotFoundException("tournament not found"));
+
+        mockMVc.perform(get("/tournament/invalid/leaderboard/top/2"))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.error").value("Tournament not found"))
+                .andExpect(jsonPath("$.message").value("tournament not found"));
     }
 
 
     @Test
-    void getLeaderboard() throws Exception {
+    void should_returnLeaderboard() throws Exception {
 
-        LeaderboardDto leaderboardDto = new LeaderboardDto(List.of(
+        LeaderboardDto expectedLeaderboard = new LeaderboardDto(List.of(
                 new LeaderboardEntryDto("Mihai", 10, 1),
                 new LeaderboardEntryDto("Alex", 8, 2),
                 new LeaderboardEntryDto("Florin", 7, 3),
@@ -55,14 +78,18 @@ class LeaderboardControllerTest {
                 new LeaderboardEntryDto("Elena", 2, 5)
         ));
 
-        when(leaderboardService.getLeaderboard("tournament1")).thenReturn(leaderboardDto);
+        when(leaderboardService.getLeaderboard("tournament1")).thenReturn(expectedLeaderboard);
 
-        mockMVc.perform(get("/tournament/tournament1/leaderboard/all"))
+        MvcResult result = mockMVc.perform(get("/tournament/tournament1/leaderboard/all"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.leaderboardEntryDtoList[0].name").value("Mihai"))
-                .andExpect(jsonPath("$.leaderboardEntryDtoList[1].score").value(8))
-                .andExpect(jsonPath("$.leaderboardEntryDtoList[2].rank").value(3))
-                .andExpect(jsonPath("$.leaderboardEntryDtoList[3].name").value("Andrei"))
-                .andExpect(jsonPath("$.leaderboardEntryDtoList[4].rank").value(5));
+                .andReturn();
+
+        String response = result.getResponse().getContentAsString();
+        LeaderboardDto actualLeaderboard = objectMapper.readValue(response, LeaderboardDto.class);
+
+
+        assertThat(actualLeaderboard)
+                .usingRecursiveComparison()
+                .isEqualTo(expectedLeaderboard);
     }
 }
